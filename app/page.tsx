e6f5f9dacf88f9cc176ac467bdfd29ac7bc8fcff
MohdSaquib114 <msmsaq114@gmail.com>
2025-01-20 18:11:50 +0530

    Half_way

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,196 @@
-import Image from "next/image";
+"use client"
+
+
+import { Toaster } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef, useState } from "react"
+import toast from 'react-hot-toast';
+import gsap from "gsap";
+
+
+
+type FormType = {
+    username:string;
+    roomId?:string;
+
+}
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [formData,setFormData] = useState<FormType>({username:"",roomId:""})
+    const [webSocket,setWebSocket] = useState<WebSocket|null>(null)
+    const [loading,setLoading] = useState(false)
+    const router = useRouter()
+    const canRef = useRef<HTMLCanvasElement>(null)
+    const headingRef = useRef(null)
+    const introRef = useRef(null)
+    const formRef = useRef(null)
+    // const [isDrwaing,setIsDrawing] = useState(false)
+     
+useEffect(()=>{
+  
+  gsap.from(headingRef.current, {
+    opacity:0,
+    y:-20,
+    duration:1
+  })
+  gsap.from(introRef.current, {
+    opacity:0,
+    x:-20,
+    duration:1
+  })
+  gsap.from(formRef.current, {
+    opacity:0,
+    x:20,
+    duration:1
+  })
+ 
+  return () =>{
+   
+}
+},
+[]
+)
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    useEffect(()=>{
+     const ws = new WebSocket("ws://localhost:8080")
+     setWebSocket(ws)
+      ws.onmessage = (e) => {
+        const response = JSON.parse(e.data)
+        if(response.success){
+           if(response.roomId){
+            
+           }
+            router.push("/room")
+            setLoading(false)
+        }
+        
+      }
+     return () => {
+        ws.close();
+        console.log("connection closed")
+      };
+    },[router])
+
+    useEffect(() => {
+      const canvas = canRef.current;
+      if (!canvas) return;
+    
+      const context = canvas.getContext("2d");
+      if (!context) return;
+    
+      // Set the drawing context
+      context.strokeStyle = "black";
+      context.lineWidth = 1;
+      context.lineCap = "round";
+    
+      
+    
+      
+      const draw = (e: MouseEvent) => {
+   
+      
+    const rect = canvas.getBoundingClientRect();
+        context.lineTo(e.clientX - rect.left, e.clientY - rect.top)
+        context.stroke()
+      
+        context.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop)
+      };
+    
+      
+    
+   
+      canvas.addEventListener("mousemove", draw);
+      
+    
+  
+      return () => {
+   
+        canvas.removeEventListener("mousemove", draw);
+      
+      };
+    }, []);
+    
+
+    const createRoom = (e: React.MouseEvent<HTMLButtonElement>)=>{
+           try {
+            e.preventDefault()
+              if(formData.username){
+                webSocket?.send(JSON.stringify({
+                    type:"createRoom",
+                    payload:{
+                        username:formData.username
+                    }
+                }))
+                setLoading(true)
+               return 
+              }
+            toast("Username field is empty")
+           } catch (error) {
+               alert(error)
+               console.log(error)
+           }
+    }
+    const joinRoom = (e: React.MouseEvent<HTMLButtonElement>)=>{
+           try {
+            console.log("clicked")
+            e.preventDefault()
+              if(formData.username && formData.roomId){
+                webSocket?.send(JSON.stringify({
+                    type:"addUser",
+                    roomId:formData.roomId,
+                    payload:{
+                        username:formData.username,
+                        
+                    }
+                }))
+                setLoading(true)
+               return 
+              }
+            toast("Username or room Id field is empty")
+           } catch (error) {
+               alert(error)
+               console.log(error)
+           }
+    }
+  return (
+    <div className="h-screen flex relative  bg-white   bg-grid-black/[0.2] 
+     
+      items-center justify-center">
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_50%,black)]"></div>
+     <canvas
+     ref={canRef}
+     className="absolute h-full w-full   border-4 border-black"
+     width={800}
+     height={600}
+     />
+    <div className=" flex flex-col gap-20  " >
+      <h1 ref={headingRef} className="text-7xl font-extrabold text-center select-none">Collaborative White Board</h1>
+
+
+    
+   <div className="grid sm:grid-cols-3 mx-20  p-10 select-none">
+
+      <div ref={introRef} className="space-y-5 col-span-2 px-10 self-center">
+        <h2 className="text-3xl font-bold">Unleash Your Creativity with Our Online Whiteboard</h2>
+        <p className="text-xl text-slate-700 font-semibold ">Welcome to your ultimate collaborative space where ideas flow freely, and creativity knows no bounds. Whether you are brainstorming, teaching, designing, or managing tasks, our whiteboard has you covered.</p>
+      </div>
+     
+        <form ref={formRef} className="flex flex-col  space-y-6 p-10  z-50 border rounded-md shadow-2xl bg-white/75  saturate-100 backdrop-blur-sm backdrop-opacity-55">
+            <h3 className="text-center text-lg font-medium" >Join or Create A Room</h3>
+                <input className=" text-sm text-slate-500 font-medium focus:outline-none border-2 px-2 py-3 rounded-md " onChange={(e)=>setFormData({...formData,username:e.target.value})} value={formData.username}  title="username" type="text" placeholder="Username" />
+                <input className=" text-sm text-slate-500 font-medium focus:outline-none border-2 px-2 py-3 rounded-md " onChange={(e)=>setFormData({...formData,roomId:e.target.value})} value={formData.roomId} title="room-id" type="text" placeholder="Room ID (required for joining)" />
+                <div className="text-slate-100 font-medium flex gap-4 justify-center">
+                    <button className="rounded-md bg-green-500 p-1  px-4 " disabled={loading} onClick={joinRoom}>Join Room</button>
+                    <button className="rounded-md bg-blue-500 p-1 px-4" disabled={loading} onClick={createRoom}>Create Room</button>
+                </div>
+        </form>
+   
+   </div>
+   
+      
+    </div>
+   <Toaster/>
     </div>
   );
 }
---
+++ components/Context.tsx
@@ -0,0 +1,37 @@
+import React, { createContext, useContext, useState, ReactNode } from "react";
+
+
+
+interface RoomContextType {
+  host: string | null;
+  roomId: string | null;
+  setHost: (host: string) => void;
+  setRoomId: (roomId: string) => void;
+}
+
+
+
+const RoomContext = createContext<RoomContextType | undefined>(undefined);
+
+
+
+export const RoomProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [host, setHost] = useState<string | null>(null);
+  const [roomId, setRoomId] = useState<string | null>(null);
+
+  return (
+    <RoomContext.Provider value={{ host, roomId, setHost, setRoomId }}>
+      {children}
+    </RoomContext.Provider>
+  );
+};
+
+
+
+export const useRoomContext = (): RoomContextType => {
+  const context = useContext(RoomContext);
+  if (!context) {
+    throw new Error("useRoomContext must be used within a RoomProvider");
+  }
+  return context;
+};
